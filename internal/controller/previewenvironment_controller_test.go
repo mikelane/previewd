9e93170c78058332a15168df08429347918c7587
Mike Lane <mikelane@gmail.com>
2025-11-09 12:34:33 -0800

    fix(controller): add required fields to controller test PreviewEnvironment

--- internal/controller/previewenvironment_controller_test.go
+++ internal/controller/previewenvironment_controller_test.go
@@ -49,7 +49,11 @@ var _ = Describe("PreviewEnvironment Controller", func() {
 						Name:      resourceName,
 						Namespace: "default",
 					},
-					// TODO(user): Specify other spec details if needed.
+					Spec: previewv1alpha1.PreviewEnvironmentSpec{
+						Repository: "test-owner/test-repo",
+						PRNumber:   123,
+						HeadSHA:    "1234567890abcdef1234567890abcdef12345678",
+					},
 				}
 				Expect(k8sClient.Create(ctx, resource)).To(Succeed())
 			}
