12e2fb3a62f0ce2613d7cc4b3417e21249e390ab
Mike Lane <mikelane@gmail.com>
2025-11-18 05:46:02 -0800

    fix: format code with gofmt and goimports

--- internal/controller/previewenvironment_controller.go
+++ internal/controller/previewenvironment_controller.go
@@ -30,10 +30,10 @@ import (
 
 	previewv1alpha1 "github.com/mikelane/previewd/api/v1alpha1"
 	"github.com/mikelane/previewd/internal/cost"
+	corev1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/api/meta"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
